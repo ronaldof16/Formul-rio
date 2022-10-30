caab9c6713df9a1ba6fe44acc686d99aa7e31a80
Ronaldo Fernando <ronaldof16@hotmail.com>
2022-10-29 23:54:56 -0300

    ´Declaração das variáveis do script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+let form = document.querySelector('#formulario');
+let campos = document.querySelectorAll('.dados');
+let msg = document.querySelectorAll('alerta');
+let validarEmail = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
-
+function validarNome() {
+    console.log('ronaldo')
+}
 
 
